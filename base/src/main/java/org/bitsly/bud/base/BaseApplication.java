5d7c6b37d75d74d6652f08b1db9e90ddfe96773f
bitsly <bitsly@163.com>
2019-06-22 19:50:34 +0800

    调整目录

--- base/src/main/java/org/bitsly/bud/base/BaseApplication.java
+++ base/src/main/java/org/bitsly/bud/base/BaseApplication.java
@@ -3,7 +3,7 @@ package org.bitsly.bud.base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.bitsly.bud.common", "org.bitsly.bud.base"})
 public class BaseApplication {
 
     public static void main(String[] args) {
--- manager/src/main/java/org/bitsly/bud/manager/ManagerApplication.java
+++ manager/src/main/java/org/bitsly/bud/manager/ManagerApplication.java
@@ -3,7 +3,7 @@ package org.bitsly.bud.manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.bitsly.bud.common", "org.bitsly.bud.manager"})
 public class ManagerApplication {
 
 	public static void main(String[] args) {
